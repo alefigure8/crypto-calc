ca66534e3d2d01d471eed6f21aef0521b901af1d
ale_figure8 <gomeznieto@gmail.com>
2022-08-13 22:17:48 -0300

    Add news section

---
+++ src/components/News.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import Cards from './Cards'
+
+const Grid = styled.div`
+margin: 0 auto;
+@media (min-width: 792px) {
+  width: 90%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+}
+`
+
+
+const News = ({news}) => {
+  return (
+    <Grid>
+      {
+        news.map(item => ( <Cards key={item.title} item={item} />))
+      }
+    </Grid>
+  )
+}
+
+export default News
