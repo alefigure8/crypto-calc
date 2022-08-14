734738adbebf1e04e22966f8b11a3e199a9a33d2
ale_figure8 <gomeznieto@gmail.com>
2022-08-13 22:18:12 -0300

    Add news cards component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Card = styled.div`
+margin: 0 auto;
+width: 95%;
+gap: 1rem;
+color: #000;
+background-color: #fff;
+border-radius: 20px;
+margin-bottom: 1rem;
+overflow: hidden;
+transition: all 0.3s ease-in-out;
+&:hover{
+  cursor: pointer;
+  transform: translate( 0px, 5px);
+}
+@media (min-width: 992px) {
+  margin-bottom: 0rem;
+  display: flex;
+  width: 90%;
+}
+`
+
+const CardContainer = styled.div`
+color: #000;
+background-color: #fff;
+border-radius: 20px;
+margin: 0 auto;
+padding: 1rem;
+gap: 1rem;
+`
+const Title = styled.h3`
+margin-top: 1rem;
+font-size: 17px;
+@media (min-width: 992px) {
+  margin: 0;
+}
+`
+
+const Parraph = styled.p`
+font-size: 15px;
+`
+
+const Btn = styled.a`
+color: #000;
+font-size: 15px;
+text-decoration: none;
+font-weight: 700;
+display: block;
+text-align: center;
+margin-bottom: 1rem;
+&:hover{
+    color: #333;
+    text-decoration: underline;
+    text-decoration-thickness: 2px;
+    text-underline-offset: 0.5rem;
+}
+@media (min-width: 992px) {
+  text-align: left;
+}
+`
+
+const Image = styled.img`
+width: 100%;
+min-height: 200px;
+max-height: 200px;
+object-fit: cover;
+object-position: center center;
+@media (min-width: 992px) {
+  max-width: 120px;
+  min-height: 100%;
+}
+`
+
+const Cards = ({item}) => {
+  return (
+      <Card key={item.id}>
+        <Image src={item.image} alt="{item.title}" />
+        <CardContainer>
+          <Title>{item.title}</Title>
+          <Parraph>{item.description.substr(0, 100)}...</Parraph>
+          <Btn href={item.url} target="_blank">Read More</Btn>
+        </CardContainer>
+      </Card>
+
+    )
+}
+
+export default Cards
